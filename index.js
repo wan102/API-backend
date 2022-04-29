197d8c24743ab2bec0d2b7e93ef7f7cfdb55c084
timw <102154201+wan102@users.noreply.github.com>
2022-04-29 02:43:25 +0000

    prototype

---
+++ index.js
@@ -0,0 +1,26 @@
+// My blog API
+// Set up the application and its router
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const app = new Koa();
+const router = new Router();
+
+/*
+* Define route handler(s):
+*
+* This means we connect HTTP methods: GET, POST, ...
+* and URI paths: /some/uri/path
+* to JavaScript functions that handle the request.
+*
+* Once defined we then add them to the app object.
+*/ router.get('/api/v1', welcomeAPI);
+app.use(router.routes());
+// Define the actual handler functions
+function welcomeAPI(ctx, next) {
+  ctx.body = {
+    message: "Welcome to the blog API!"
+  }
+}
+// Finally, run the app as a process on a given port
+app.listen(3000);
